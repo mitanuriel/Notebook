991ac7820cc3b477cea17c9ed9ca1aa587172f9f
mitanuriel <145558996+mitanuriel@users.noreply.github.com>
2025-03-24 18:28:58 +0100

    Added navigation dependencies and imports

--- App.js
+++ App.js
@@ -1,4 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { useState } from 'react';
 import { StyleSheet,FlatList, Button, View, TextInput, Text } from 'react-native';
 
