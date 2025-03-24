33b5898cc62e246f48d06ef32badf1728649ace1
mitanuriel <145558996+mitanuriel@users.noreply.github.com>
2025-03-24 17:58:05 +0100

    Added TextInput

--- App.js
+++ App.js
@@ -1,13 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Button, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Button, View, TextInput } from 'react-native';
 
 export default function App() {
 
+  const [text, setText] = useState('')//gotta survive the rerendering, hook as a reigniting mechanism
+
   function buttonHandler(){
-    alert("you're good")
+    alert("you types:" + text)
   }
   return (
     <View style={styles.container}>
+      <TextInput onChangeText={(txt) => setText(txt)}/>
       <Button title='Press me' onPress={buttonHandler}></Button>
       <StatusBar style="auto" />
     </View>
